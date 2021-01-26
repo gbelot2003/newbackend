f87fc02e6d1bcf6b74e0db01824659a670daee7a
Gerardo Belot <gerardo.belot@gmail.com>
2021-01-25 23:35:55 -0600

    Adding laravel trix and setting configurations for jetstram pages

---
+++ app/Http/Livewire/Pages.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Page;
+use Livewire\Component;
+
+class Pages extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages');
+    }
+}
